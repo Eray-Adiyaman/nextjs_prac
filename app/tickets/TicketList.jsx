d91764f72464f7f7ecf61c8b1736a3b841ecc498
Eray-Adiyaman <erayadiyaman@outlook.com>
2023-10-09 15:53:28 +0300

    dynamic segments(params)

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,31 +1,33 @@
+import Link from "next/link";
+
 async function getTickets() {
-    const res = await fetch("http://localhost:4000/tickets", {
-        next: {
-            revalidate: 0 // no caching data
-        }
-    });
-  
-    return res.json();
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: {
+      revalidate: 0, // no caching data
+    },
+  });
+
+  return res.json();
 }
 
 export default async function TicketList() {
-    const tickets = await getTickets();
+  const tickets = await getTickets();
   return (
-        <>
-         {tickets.map((ticket)=> (
-            <div key={ticket.id} className="card my-5">
-                <h3>{ticket.title}</h3>
-                <p>{ticket.body.slice(0,200)}...</p>
-                <div className={`pill ${ticket.priority}`}>
-                    {ticket.priority} priority
-                </div>
-               
-
+    <>
+      {tickets.map((ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
             </div>
-         ))}
-         {tickets.length === 0 && (
-            <p className="text-center">There are no open tickets!</p>
-         )}
-        </>
-    )
+          </Link>
+        </div>
+      ))}
+      {tickets.length === 0 && (
+        <p className="text-center">There are no open tickets!</p>
+      )}
+    </>
+  );
 }
