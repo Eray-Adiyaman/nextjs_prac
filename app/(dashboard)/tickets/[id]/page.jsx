351aebd56d6b64d76a83caae5a466c83c3a025bd
Eray-Adiyaman <erayadiyaman@outlook.com>
2023-10-25 16:48:45 +0300

    session middleware

--- app/(dashboard)/tickets/[id]/page.jsx
+++ app/(dashboard)/tickets/[id]/page.jsx
@@ -27,7 +27,7 @@ export async function generateStaticParams() {
 async function getTicket(id) {
     const res = await fetch("http://localhost:4000/tickets/" + id, {
         next: {
-            revalidate: 30
+            revalidate: 0
         }
     });
 
